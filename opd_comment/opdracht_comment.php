6d8e025d873f42ed26247aa85e7251f35a03f489
Maurits Robbe <Maurits Robbe>
2016-10-06 15:10:51 +0200

    opdracht 1

---
+++ opd_comment/opdracht_comment.php
@@ -0,0 +1,20 @@
+<?php
+    /*
+        André Maurits Robbe
+        andre.robbe@student.kdg.be
+    */
+    $naam = 'André Maurits Robbe';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>comment</title>
+    <h1>Oplossing: comments</h1>
+    <p>Ik ben <?= $naam ?></p>
+</head>
+<body>
+    
+</body>
+</html>
